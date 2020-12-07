28a167f948fe4416002f061743a926073d0c28a5
joengelh <73387330+joengelh@users.noreply.github.com>
2020-12-07 20:14:47 +0100

    automated commit for update saving

--- runner.sh
+++ runner.sh
@@ -1,2 +1,3 @@
-sudo docker run -d -p 22:2222 joengelh/dockerhub-ubuntuserver:latest
+sudo docker run -d -P joengelh/dockerhub-ubuntuserver:latest
 container_name=$(sudo docker ps -a | head -n 2 | tail -n 1 | grep -oE '[^ ]+$')
+sudo docker port $container_name 22
