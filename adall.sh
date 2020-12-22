da1b0882e84b5fad314d189356d45d9d880c892f
joengelh <73387330+joengelh@users.noreply.github.com>
2020-12-22 17:52:52 +0100

    automated commit for update saving

--- adall.sh
+++
@@ -1,7 +0,0 @@
-git config --global user.email "73387330+joengelh@users.noreply.github.com"
-git config --global user.name "joengelh"
-git pull
-git add -u
-git add *
-git commit -m 'automated commit for update saving'
-git push
--- adnpush.sh
+++ adnpush.sh
@@ -1,3 +1,10 @@
+git config --global user.email "73387330+joengelh@users.noreply.github.com"
+git config --global user.name "joengelh"
+git pull
+git add -u
+git add *
+git commit -m 'automated commit for update saving'
+git push
 image_name=$(sudo docker build . | tail -n 1 | grep -oE '[^ ]+$')
 container_name=$(sudo docker create $image_name)
 sudo docker commit $container_name joengelh/dockerhub-ubuntuserver
